cbe97848ed2e7ff53106406a587b9eb54bc826ee
Daniel Leal <daniel.suportte@gmail.com>
2023-07-26 00:26:10 -0300

    Exercicio de 1 a 5

--- Aulas/250723.js
+++ Aulas/250723.js
@@ -1,2 +1,45 @@
 const prompt = require("prompt-sync")();
 
+//Exercícios sala de aula
+
+function exercicio1() {
+    console.log("Olá Mundo!");
+    
+}
+
+function exercicio2() {
+    const userName = prompt("Digite seu nome aqui: ");
+    console.log("Seu nome de guerra é: " + userName);
+}
+
+function exercicio3() {
+    const number1 = parseInt(prompt("Digite um número inteiro: "));
+    const number2 = parseInt(prompt("Digite outro número inteiro: "));
+
+    const result = number1 + number2;
+
+    console.log("A soma dos números digitados é: " + result);
+}
+
+function exercicio4() {
+    const idade = parseInt(prompt("Digite aqui a sua idade: "));
+
+    if (idade >= 18) {
+        console.log("Você já e maior de idade! xD ");
+    } else {
+        console.log("Você ainda é de menor. . . =/ ");
+    }
+}
+
+function exercicio5() {
+    const number = parseInt(prompt("Digite aqui um número para saber se ele é par ou ímpar: "));
+
+    if (number % 2 === 0) {
+        console.log("O número " + number + "que você digitou é par!");
+    } else {
+        console.log("O número " + number + "que você digitou é ímpar!");
+    }
+}
+
+
+exercicio5();
