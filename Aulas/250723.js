51094657b2f8f25616d9df0342ea41c839e0e8ef
Daniel Leal <daniel.suportte@gmail.com>
2023-07-27 19:31:25 -0300

    Exercicio 8

--- Aulas/250723.js
+++ Aulas/250723.js
@@ -57,6 +57,24 @@ function exercicio6() {
 }
 
 function exercicio7() {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i);
+        
+    }
+
+}
+
+function exercicio8() {
+    console.log("Aqui estão os números pares de 1 a 20: ");
+    for (let i = 1; i <= 20; i++) {
+        if (i % 2 === 0) {
+        console.log(i);
+        }
+    }
+}
+
+function exercicio9() {
     
 }
-exercicio6();
+
+exercicio8();
