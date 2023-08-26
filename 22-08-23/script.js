104386bbc67b60a2e0139895d001bf95f8026472
Daniel Leal <daniel.suportte@gmail.com>
2023-08-26 16:34:37 -0300

    att final

--- 22-08-23/script.js
+++ 22-08-23/script.js
@@ -1,14 +1,36 @@
-const botao = document.getElementById("botao1");
-const input = document.getElementById("input1");
+const btEnviar = document.getElementById("btEnviar");
+const lista = document.getElementById("minha-lista");
+const inpNome = document.getElementById("inpNome");
+const btExcluir = document.getElementById("btExcluir");
 
-botao.addEventListener("click", lidarComClique);
+btEnviar.addEventListener("click", adicionar);
+btExcluir.addEventListener("click", excluir);
 
-function lidarComClique(evento) {
-    const valorDoCampo = input.value;
-    alert(valorDoCampo);
+const listaDePessoas = [];
+
+function adicionar() {
+  const valorDoCampo = inpNome.value;
+  listaDePessoas.push(valorDoCampo);
+  renderizarLista();
+  inpNome.value = "";
 }
 
-function listarAlunos() {
-    let alunos = [];
-    const nome = prompt(`Insira um nome`);
+function renderizarLista() {
+  lista.innerHTML = "";
+  for (let i = 0; i < listaDePessoas.length; i++) {
+    const pessoa = listaDePessoas[i];
+    const itemDaLista = document.createElement("li");
+    itemDaLista.innerHTML = itemDaLista.innerHTML = pessoa;
+    lista.appendChild(itemDaLista);
+  }
+}
+
+function excluir() {
+  const valorDoCampo = inpNome.value;
+  const valorDoCampoExcluir = listaDePessoas.indexOf(valorDoCampo);
+  if (valorDoCampoExcluir !== -1) {
+    listaDePessoas.splice(valorDoCampoExcluir, 1);
+    renderizarLista();
+  }
+  inpNome.value = "";
 }
