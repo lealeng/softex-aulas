b0e591396206742fe0120ecfab2b967d2befa425
Daniel Leal <daniel.suportte@gmail.com>
2023-09-12 21:01:05 -0300

    att npm and update model user (add field password)

--- controle/src/routes/category.ts
+++ controle/src/routes/category.ts
@@ -1,7 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
 import { z } from 'zod'
-import { request } from 'http'
 
 export async function categoryRoutes(app: FastifyInstance) {
   app.get('/categories', async (request, reply) => {
