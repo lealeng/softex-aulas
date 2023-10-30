5d1c593c3f8a283d7ce31736ce5c3ee59bfc604e
Daniel Leal <daniel.suportte@gmail.com>
2023-10-30 13:41:50 -0300

    v4.0 - correção post createProduct - funcionando

--- mkt-softex/src/controllers/product/product.controller.post.ts
+++ mkt-softex/src/controllers/product/product.controller.post.ts
@@ -1,46 +1,77 @@
 import { Request, Response } from "express";
 import { prisma } from "../../lib/prisma";
-import { z } from "zod";
+import { z, ZodError } from "zod";
 
 class ProductControllerPost {
-    async createProduct(req: Request, res: Response) {
-        try {
-            const bodySchema = z.object({
-                codBars: z.string(),
-                name: z.string(),
-                price: z.number(),
-                activated: z.boolean().default(true),
-                categoryId: z.number(),
-                subCategoryId: z.number(),
-                userId: z.number(),
-            })
-            const {
-                codBars,
-                name,
-                price,
-                activated,
-                categoryId,
-                subCategoryId,
-                userId,
-            } = bodySchema.parse(req.body)
+  async createProduct(req: Request, res: Response) {
+    try {
+      const bodySchema = z.object({
+        codBars: z.string(),
+        name: z.string(),
+        price: z.number(),
+        categoryId: z.number(),
+        subCategoryId: z.number(),
+        userId: z.number().int(),
+      });
 
-            const product = await prisma.product.create({
-                data: {
-                    codBars,
-                    name,
-                    price,
-                    activated,
-                    categoryId,
-                    subCategoryId,
-                    userId,
-                }
-            })
+      // Verificar a validade dos dados
+      const validatedData = bodySchema.parse(req.body);
 
-            return res.status(201).json(product)
-        } catch (error) {
-            return res.status(500).json({ message: "Erro ao criar o produto, tente novamente" });
-        }
+      const { codBars, name, price, categoryId, subCategoryId, userId } =
+        validatedData;
+
+      const category = await prisma.category.findUnique({
+        where: { id: categoryId },
+      });
+      const subCategory = await prisma.subCategory.findUnique({
+        where: { id: subCategoryId },
+      });
+      const existingProduct = await prisma.product.findUnique({
+        where: { codBars },
+      });
+
+      if (!category) {
+        return res
+          .status(400)
+          .json({ message: "A categoria informada não existe." });
+      }
+
+      if (!subCategory) {
+        return res
+          .status(400)
+          .json({ message: "A subcategoria informada não existe." });
+      }
+
+      if (existingProduct) {
+        return res.status(400).json({
+          message: "A mercadoria com o código de barras informado já existe.",
+        });
+      }
+
+      const newProduct = await prisma.product.create({
+        data: {
+          codBars,
+          name,
+          price,
+          categoryId,
+          subCategoryId,
+          userId,
+        },
+      });
+
+      return res.status(201).json(newProduct);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return res
+          .status(400)
+          .json({ message: "Erro de validação", error: error.errors });
+      }
+
+      return res
+        .status(500)
+        .json({ message: "Erro ao criar o produto, tente novamente." });
     }
+  }
 }
 
 export default new ProductControllerPost();
